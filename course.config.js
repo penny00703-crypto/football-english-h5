07171f739a82044f45dc8f67a376e70687d557be
penny00703-crypto <penny00703@gmail.com>
2026-07-22 17:20:08 +0800

    Publish Football English interactive H5

---
+++ course.config.js
@@ -0,0 +1,14 @@
+window.COURSE_CONFIG = {
+  brandName: "Talk Academy",
+  lessonTitle: "Football English",
+  lessonSubtitle: "Speak like you belong on the pitch",
+  level: "A2–B1",
+  durationMinutes: 25,
+  locale: "en",
+  teacherHintLanguage: "en",
+  accent: "#ffd43b",
+  showBrand: true,
+  soundOn: true,
+  shareMode: "global",
+  version: "2.2.0-mini-challenge"
+};
